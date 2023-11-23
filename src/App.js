0ca81558c195f762c405ff36bbe4ce4f5e5f54c3
Michael Salerno <msalerno1993@gmail.com>
2023-11-23 18:20:31 -0500

    socials section completed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import Home from './components/Home';
 import NavBar from './components/NavBar';
+import Socials from './components/Socials';
 import './index.css'
 
 function App() {
   return (
     <div>
       <NavBar />
-      <div>
         <Home />
-      </div>
+        <Socials />
     </div>
   );
 }
--- src/assets/Data.jsx
+++ src/assets/Data.jsx
@@ -1,7 +1,54 @@
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+import { BsFillPersonLinesFill } from "react-icons/bs";
+
 export const navLinks = [
-    { id: 1, name: "Home", link: "home" },
-    { id: 2, name: "About", link: "about" },
-    { id: 3, name: "Portfolio", link: "portfolio" },
-    { id: 4, name: "Technology", link: "technology" },
-    { id: 5, name: "Contact", link: "contact" },
-  ];
+  { id: 1, name: "Home", link: "home" },
+  { id: 2, name: "About", link: "about" },
+  { id: 3, name: "Portfolio", link: "portfolio" },
+  { id: 4, name: "Technology", link: "technology" },
+  { id: 5, name: "Contact", link: "contact" },
+];
+
+
+export const socials = [
+  {
+    id: 1,
+    child: (
+      <>
+        LinkedIn <FaLinkedin size={35} />
+      </>
+    ),
+    href: "https://linkedin.com",
+    style: "rounded-tr-md",
+  },
+  {
+    id: 2,
+    child: (
+      <>
+        GitHub <FaGithub size={35} />
+      </>
+    ),
+    href: "https://github.com/theyashpatel",
+  },
+  {
+    id: 3,
+    child: (
+      <>
+        Mail <HiOutlineMail size={35} />
+      </>
+    ),
+    href: "mailto:foo@gmail.com",
+  },
+  {
+    id: 4,
+    child: (
+      <>
+        Resume <BsFillPersonLinesFill size={35} />
+      </>
+    ),
+    href: "/resume.pdf",
+    style: "rounded-br-md",
+    download: true,
+  },
+]
