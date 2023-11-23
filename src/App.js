db967eff8a439a4406dfbe627644c34df78e8e55
Michael Salerno <msalerno1993@gmail.com>
2023-11-23 16:11:02 -0500

    entire navbar complete

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Home from './components/Home';
 import NavBar from './components/NavBar';
 import './index.css'
 
@@ -5,6 +6,7 @@ function App() {
   return (
     <div>
       <NavBar />
+      <Home />
     </div>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,11 @@
 import { FaBars, FaTimes } from "react-icons/fa";
 import { navLinks } from "../assets/Data";
+import { useState } from "react";
+import { Link } from "react-scroll";
 
 const NavBar = () => {
+  const [nav, setNav] = useState(false);
+
   return (
     <div className="flex justify-between items-center w-full h-20 px-4 text-white fixed bg-gray-900">
       <div>
@@ -21,6 +25,32 @@ const NavBar = () => {
           );
         })}
       </ul>
+      <div
+        onClick={() => setNav(!nav)}
+        className="cursor-pointer pr-4 z-10 text-gray-300 md:hidden"
+      >
+        {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
+      </div>
+
+      {nav && (
+        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-gray-900 to-gray-700 text-gray-300">
+          {navLinks.map(({ id, name, link }) => (
+            <li
+              key={id}
+              className="px-4 cursor-pointer capitalize py-6 text-4xl text-gray-300 hover:scale-110 duration-200"
+            >
+              <Link
+                onClick={() => setNav(!nav)}
+                to={link}
+                smooth
+                duration={500}
+              >
+                {name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
